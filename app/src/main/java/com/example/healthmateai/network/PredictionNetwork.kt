ed99e683e59ccf9cd139a7f3486e5a920b8315df
thedhanush0905 <dhanushmaddila0905@gmail.com>
2026-05-07 13:16:23 +0530

    Switch API base URL to Render production

--- app/src/main/java/com/example/healthmateai/network/PredictionNetwork.kt
+++ app/src/main/java/com/example/healthmateai/network/PredictionNetwork.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 import java.util.concurrent.TimeUnit
 
-private const val BASE_URL = "http://10.0.2.2:5000/"
+private const val BASE_URL = "https://healthmateai-mlds.onrender.com/"
 
 interface PredictionApiService {
     @POST("predict")
